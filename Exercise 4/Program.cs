908a751af051501cd4e090ec1cc47e4870381495
panjialbir <panjialbir10@gmail.com>
2022-12-08 16:02:40 +0700

    2

--- Exercise 4/Program.cs
+++ Exercise 4/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+
+namespace StackUsing
+{
+    class Stack
+    {
+        private int[] ele;
+        private int top;
+        private int max;
+        public Stack(int size)
+        {
+            ele = new int[size];
+            top = -1;
+            max = size;
+        }
+
+        public void push(int item)
+        {
+            if (top == max - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            else
+            {
+                ele[++top] = item;
+            }
+        }
